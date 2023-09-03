413b2575adafce7068e6a9bdb2ab130692d58f5c
hassannh <hassannouhi91@gmail.com>
2023-09-03 06:13:48 +0100

    data.sql file for tqbles

---
+++ src/data.sql
@@ -0,0 +1,21 @@
+use mini_sas;
+
+CREATE TABLE user (
+        id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+
+
+
+CREATE TABLE movies (
+        id INT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        author VARCHAR(255) NOT NULL
+    );
+
+
+
+
+
