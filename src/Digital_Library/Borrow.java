d8521ac77708e4183f7ea2a6e99ee6d0c178d3b3
hassannh <hassannouhi91@gmail.com>
2023-09-06 20:53:57 +0100

    member functions

--- src/Digital_Library/Borrow.java
+++ src/Digital_Library/Borrow.java
@@ -1,8 +1,48 @@
 package Digital_Library;
 
+import java.util.List;
+
 public class Borrow {
 
+String date_p;
+String date_r;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getDate_p() {
+        return date_p;
+    }
+
+    public void setDate_r(String title) {
+        this.date_r = date_r;
+    }
 
 }
